483d296c5c6f0179207787cef1cdfdee402de4e6
Sudhanshu Barnwal <83909429+Sudhanshu3314@users.noreply.github.com>
2025-07-15 12:36:18 +0530

    CMMS

--- Backend/Controllers/AuthController.js
+++ Backend/Controllers/AuthController.js
@@ -1,14 +1,23 @@
 const UserModel = require("../models/user_model")
+const bcrypt = require('bcrypt')
 
 const signup = async (req, res) => {
     try {
         const { name, email, password } = req.body
         const user = await UserModel.findOne({ email });
-    } catch (err) {
+        if (user) {
+            return res.status(409).json({ message: "User is already exist, you can login", success: false });
+        }
+        const userModel = new UserModel({ name, email, password });
+        userModel.password = await bcrypt.hash(password, 10);
+        await userModel.save();
+        return res.status(201).json({ message: "Signup successfully", success: true });
 
+    } catch (err) {
+        return res.status(500).json({ message: "Internal Server Error", success: true });
     }
 }
 
 module.exports = {
     signup
-}
+} 
