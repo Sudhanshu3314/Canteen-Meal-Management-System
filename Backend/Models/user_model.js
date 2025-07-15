cb4170698c38842dc00b9124cd05b16d9ab25878
Sudhanshu Barnwal <83909429+Sudhanshu3314@users.noreply.github.com>
2025-07-15 12:38:42 +0530

    Create user_model.js

---
+++ Backend/Models/user_model.js
@@ -0,0 +1,30 @@
+// models/User.js
+
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,  // Ensures email is unique
+        lowercase: true,
+        trim: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 8 
+    }
+}, {
+    timestamps: true // adds createdAt and updatedAt fields automatically
+});
+
+const UserModel = mongoose.model('User', UserSchema);
+
+module.exports = UserModel;
